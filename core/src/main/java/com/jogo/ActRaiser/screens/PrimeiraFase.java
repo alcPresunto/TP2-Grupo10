415ad5e0326a876231e40b0568624174b4b675da
alcPresunto <guilhermeasouza906@gmail.com>
2025-07-20 10:55:48 -0300

    Colisao do player-inimigo pronta

--- core/src/main/java/com/jogo/ActRaiser/screens/PrimeiraFase.java
+++ core/src/main/java/com/jogo/ActRaiser/screens/PrimeiraFase.java
@@ -4,25 +4,18 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector2;
 import com.jogo.ActRaiser.GameRunner;
 import com.jogo.ActRaiser.modelos.mapas.MapaPrimeiraFase;
+import com.jogo.ActRaiser.modelos.objetos.moveis.personagens.inimigos.Morcego;
 import com.jogo.ActRaiser.modelos.objetos.moveis.personagens.player.Player;
-import com.jogo.ActRaiser.GameRunner;
 import com.jogo.ActRaiser.logica.ControladorDeMoveis;
-import com.jogo.ActRaiser.modelos.mapas.MapaPrimeiraFase;
-import com.jogo.ActRaiser.modelos.objetos.moveis.personagens.player.Player;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.math.Vector2;
 
 public class PrimeiraFase implements Screen {
     private final GameRunner gameRunner;
     private MapaPrimeiraFase mapaPrimeiraFase;
     private Player player;
+    private Morcego morcego;
     private OrthographicCamera camera;
-    private Vector2 cameraFoco;
     private ControladorDeMoveis controladorDeMoveis;
 
     public PrimeiraFase(GameRunner gameRunner) {
@@ -34,38 +27,39 @@ public class PrimeiraFase implements Screen {
         controladorDeMoveis = new ControladorDeMoveis();
         mapaPrimeiraFase = new MapaPrimeiraFase();
         player = controladorDeMoveis.criaPlayer();
+        morcego = controladorDeMoveis.criaMorcego(player);
 
         camera = new OrthographicCamera();
         // Zoom de 2x: diminui a viewport pela metade
         camera.setToOrtho(false, Gdx.graphics.getWidth() / 2f, Gdx.graphics.getHeight() / 2f);
 
-        cameraFoco = new Vector2();
     }
 
-@Override
-public void render(float delta) {
-    Gdx.gl.glClearColor(0, 0, 1, 1);
-    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-    // Atualiza a posição da câmera para o centro do player
-    camera.position.set(
-            player.getPosicaoX() + player.getTexture().getWidth() / 2f,
-            player.getPosicaoY() + player.getTexture().getHeight() / 2f,
-            0
-    );
-    camera.update();
+        // Atualiza a posição da câmera para o centro do player
+        camera.position.set(
+                player.getPosicaoX() + player.getTexture().getWidth() / 2f,
+                player.getPosicaoY() + player.getTexture().getHeight() / 2f,
+                0);
+        camera.update();
 
-    // Renderiza o mapa com a câmera atualizada
-    mapaPrimeiraFase.render(camera);
-    player.mover();
+        // Renderiza o mapa com a câmera atualizada
+        mapaPrimeiraFase.render(camera);
+        player.mover();
+        morcego.mover();
+        controladorDeMoveis.colisaoPlayer(player, morcego);
 
-    // Garante que o batch use a matriz de projeção da câmera
-    gameRunner.batch.setProjectionMatrix(camera.combined);
-    gameRunner.batch.begin();
-    gameRunner.batch.draw(player.getTexture(), player.getPosicaoX(), player.getPosicaoY());
-    gameRunner.batch.end();
-}
-
+        // Garante que o batch use a matriz de projeção da câmera
+        gameRunner.batch.setProjectionMatrix(camera.combined);
+        gameRunner.batch.begin();
+        gameRunner.batch.draw(player.getTexture(), player.getPosicaoX(), player.getPosicaoY());
+        gameRunner.batch.draw(morcego.getTexture(), morcego.getPosicaoX(), morcego.getPosicaoY());
+        gameRunner.batch.end();
+    }
 
     @Override
     public void pause() {
