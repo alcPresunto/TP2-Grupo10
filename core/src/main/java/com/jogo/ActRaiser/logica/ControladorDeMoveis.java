a26bd05dda32e532595a3615768b45a1980bf34f
alcPresunto <guilhermeasouza906@gmail.com>
2025-07-20 10:32:58 -0300

    Colisão com inimigo

--- core/src/main/java/com/jogo/ActRaiser/logica/ControladorDeMoveis.java
+++ core/src/main/java/com/jogo/ActRaiser/logica/ControladorDeMoveis.java
@@ -1,11 +1,13 @@
 package com.jogo.ActRaiser.logica;
 
+import com.badlogic.gdx.math.Rectangle;
 import com.jogo.ActRaiser.Director;
-import com.jogo.ActRaiser.modelos.objetos.moveis.Movel;
+import com.jogo.ActRaiser.modelos.objetos.ObjetoDoJogo;
 import com.jogo.ActRaiser.modelos.objetos.moveis.personagens.inimigos.Diabinho;
 import com.jogo.ActRaiser.modelos.objetos.moveis.personagens.inimigos.DiabinhoBuilder;
 import com.jogo.ActRaiser.modelos.objetos.moveis.personagens.inimigos.Dragao;
 import com.jogo.ActRaiser.modelos.objetos.moveis.personagens.inimigos.DragaoBuilder;
+import com.jogo.ActRaiser.modelos.objetos.moveis.personagens.inimigos.Inimigo;
 import com.jogo.ActRaiser.modelos.objetos.moveis.personagens.inimigos.Morcego;
 import com.jogo.ActRaiser.modelos.objetos.moveis.personagens.inimigos.MorcegoBuilder;
 import com.jogo.ActRaiser.modelos.objetos.moveis.personagens.player.Player;
@@ -14,36 +16,89 @@ import com.jogo.ActRaiser.modelos.objetos.moveis.personagens.player.PlayerBuilde
 public class ControladorDeMoveis {
     Director director = new Director();
 
-    public Player criaPlayer(){
+    public Player criaPlayer() {
         PlayerBuilder playerBuilder = new PlayerBuilder();
         director.constructorPlayer(playerBuilder);
         Player player = playerBuilder.buildPlayer();
         return player;
     }
 
-    public Morcego criaMorcego(Player player){
+    public Morcego criaMorcego(Player player) {
         MorcegoBuilder morcegoBuilder = new MorcegoBuilder();
         director.constructorMorcego(morcegoBuilder, player);
         Morcego morcego = morcegoBuilder.buildMorcego();
         return morcego;
     }
 
-    public Dragao criaDragao(Player player){
+    public Dragao criaDragao(Player player) {
         DragaoBuilder dragaoBuilder = new DragaoBuilder();
         director.constructorDragao(dragaoBuilder, player);
         Dragao dragao = dragaoBuilder.buildDragao();
         return dragao;
     }
 
-    public Diabinho criaDiabinho(Player player){
+    public Diabinho criaDiabinho(Player player) {
         DiabinhoBuilder diabinhoBuilder = new DiabinhoBuilder();
         director.constructorDiabinho(diabinhoBuilder, player);
         Diabinho diabinho = diabinhoBuilder.buildDiabinho();
         return diabinho;
     }
 
-    public void colisao(Movel objetoMovel, Movel outroObjetoMovel){
-        if (objetoMovel != outroObjetoMovel && objetoMovel.getHitbox().overlaps(outroObjetoMovel.getHitbox())) {
+    public void colisaoPlayer(Player player, ObjetoDoJogo objeto) {
+        if (player != objeto && player.getHitbox().overlaps(objeto.getHitbox())) {
+
+            if (objeto instanceof Inimigo) {
+                player.removePontosVida(((Inimigo) objeto).getPontosDano());
+
+                Rectangle playerHitbox = player.getHitbox();
+                Rectangle objetoHitbox = objeto.getHitbox();
+                float overlapX = calculaOverlapX(player, objeto);
+                float overlapY = calculaOverlapY(player, objeto);
+
+                if (overlapX < overlapY) {
+                    if (playerHitbox.x < objetoHitbox.x) {
+                        player.setPosicaoX(player.getPosicaoX() - overlapX);
+                    } else {
+                        player.setPosicaoX(player.getPosicaoX() + overlapX);
+                    }
+                } else {
+                    if (playerHitbox.y < objetoHitbox.y) {
+                        player.setPosicaoY(player.getPosicaoY() - overlapY);
+                    } else {
+                        player.setPosicaoY(player.getPosicaoY() + overlapY);
+                    }
+                }
+            }
         }
     }
+
+    private float calculaOverlapX(Player player, ObjetoDoJogo objeto) {
+        Rectangle playerHitbox = player.getHitbox();
+        Rectangle objetoHitbox = objeto.getHitbox();
+
+        float overlapX = Math.min(playerHitbox.x + playerHitbox.width - objetoHitbox.x,
+                objetoHitbox.x + objetoHitbox.width - playerHitbox.x);
+
+        return overlapX;
+    }
+
+    private float calculaOverlapY(Player player, ObjetoDoJogo objeto) {
+        Rectangle playerHitbox = player.getHitbox();
+        Rectangle objetoHitbox = objeto.getHitbox();
+
+        float overlapY = Math.min(
+                playerHitbox.y + playerHitbox.height - objetoHitbox.y,
+                objetoHitbox.y + objetoHitbox.height - playerHitbox.y);
+
+        return overlapY;
+    }
+
+    // public void colisaoInimigo(Inimigo inimigo, ObjetoDoJogo objeto){
+    //     if (inimigo != objeto && inimigo.getHitbox().overlaps(objeto.getHitbox())) {
+            
+    //         if (objeto instanceof ) {
+                
+    //         }
+    //     }
+    // }
 }
--- core/src/main/java/com/jogo/ActRaiser/screens/PrimeiraFase.java
+++ core/src/main/java/com/jogo/ActRaiser/screens/PrimeiraFase.java
@@ -4,13 +4,11 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.jogo.ActRaiser.GameRunner;
+import com.jogo.ActRaiser.logica.ControladorDeMoveis;
 import com.jogo.ActRaiser.modelos.mapas.MapaPrimeiraFase;
 import com.jogo.ActRaiser.modelos.objetos.moveis.personagens.player.Player;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.Game;
 
 public class PrimeiraFase implements Screen {
     private final GameRunner gameRunner;
@@ -18,6 +16,7 @@ public class PrimeiraFase implements Screen {
     private Player player;
     private OrthographicCamera camera;
     private Vector2 cameraFoco;
+    private ControladorDeMoveis controladorDeMoveis;
 
     public PrimeiraFase(GameRunner gameRunner) {
         this.gameRunner = gameRunner;
@@ -25,9 +24,9 @@ public class PrimeiraFase implements Screen {
 
     @Override
     public void show() {
+        controladorDeMoveis = new ControladorDeMoveis();
         mapaPrimeiraFase = new MapaPrimeiraFase();
-        player = new Player(100, 100, new Texture(Gdx.files.internal("assets/angel.png")),
-                new Rectangle(50, 50, 50, 50), 50, 200, 100, 10);
+        player = controladorDeMoveis.criaPlayer();
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
@@ -39,10 +38,11 @@ public class PrimeiraFase implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        // cameraFoco.set(player.getPosX(), player.getPosY());
-        // camera.position.set(cameraFoco, 0);camera.update();
+        cameraFoco.set(player.getPosicaoX(), player.getPosicaoY());
+        camera.position.set(cameraFoco, 0);camera.update();
         mapaPrimeiraFase.render(camera);
         gameRunner.batch.begin();
+        gameRunner.batch.draw(player.getTexture(), player.getPosicaoX(), player.getPosicaoY());
         gameRunner.batch.end();
     }
 
